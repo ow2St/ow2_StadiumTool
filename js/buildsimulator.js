c49df452ccadef6eac6339f27cc8a4e9ec1a5f4e
RIN654xyz <rin654xyz@gmail.com>
2025-06-05 22:23:52 +0900

    ・ヒーロ選択ウィンドウ追加 ・ヘッダーにトップページ追加

--- js/buildsimulator.js
+++ js/buildsimulator.js
@@ -6,3 +6,13 @@
 // ------------------------------
 // 関数部
 // ------------------------------
+
+// ヒーロー選択ウィンドウを開く
+function openHeroWindow(){
+    document.getElementById("hero-window").style.display = "block";
+}
+
+// ヒーロー選択ウィンドウを閉じる
+function closeHeroWindow(){
+    document.getElementById("hero-window").style.display = "none";
+}
