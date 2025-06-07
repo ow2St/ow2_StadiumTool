f30f4db6b8880317c2fd5e573defee1b2afb794f
RIN654xyz <rin654xyz@gmail.com>
2025-06-07 13:13:47 +0900

    ・ヒーローウィンドウとビルドボックスに仮アイコン追加

--- js/buildsimulator.js
+++ js/buildsimulator.js
@@ -9,10 +9,15 @@
 
 // ヒーロー選択ウィンドウを開く
 function openHeroWindow(){
-    document.getElementById("hero-window").style.display = "block";
+    document.getElementById("herowindow").style.display = "block";
 }
 
 // ヒーロー選択ウィンドウを閉じる
 function closeHeroWindow(){
-    document.getElementById("hero-window").style.display = "none";
+    document.getElementById("herowindow").style.display = "none";
+}
+
+// ヒーロー選択
+function selectHero(){
+    document.getElementById("herowindow").style.display = "none";
 }
