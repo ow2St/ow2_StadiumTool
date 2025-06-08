6f649b21eb2bba8cc6dbc8e6a102a64e73220dff
R-1125 <ruuto0430@icloud.com>
2025-06-09 00:59:29 +0900

    タブ分け中＆ボタン色変え

--- js/itemandpower.js
+++ js/itemandpower.js
@@ -20,9 +20,18 @@ function ow2IconClick(){
     const chat = document.getElementById("chat")
     chat.style.display = (chat.style.display === "block") ? "none" : "block";
 }
+
+const Button = 
 */
+
+/* function IAPbutton(){
+    const b=document.getElementById("itemButton");
+    b.style.backgroundColor = "lightgray";
+        document.getElementById("itemButton").style.backgroundColor = "lightgray";
+} */
+/*TODO:カラーが反映されない*/
 
 function IAPbutton(){
     const Button = document.getElementById("itemButton")
-    button.style.backgroundColor = (button.style.backgroundColor === "#fff") ? "#ffffff80" : "#fff";
-}/*TODO:カラーが反映されない*/
+    Button.style.backgroundColor = (Button.style.backgroundColor === "#D3D3D3") ? "FFFFFF" : "#D3D3D3";
+}
