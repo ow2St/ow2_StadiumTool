d3dd261415405304d5181c6033aed26fe020e445
R-1125 <ruuto0430@icloud.com>
2025-06-13 00:04:07 +0900

    ボタン色変え完成

--- js/itemandpower.js
+++ js/itemandpower.js
@@ -4,8 +4,6 @@ function ow2IconClick(){
     const chat = document.getElementById("chat")
     chat.style.display = (chat.style.display === "block") ? "none" : "block";
 }
-
-const Button = 
 */
 
 /* function IAPbutton(){
@@ -13,10 +11,11 @@ const Button =
     b.style.backgroundColor = "lightgray";
         document.getElementById("itemButton").style.backgroundColor = "lightgray";
 } */
-let isCheckWeapon = true;
 
-//武器ボタン選択カラー変更
-function IAPbutton(){
+//ボタン選択カラー変更
+//武器
+let isCheckWeapon = true;
+function IAPbuttonWeapon(){
     var Button = document.getElementById("itemButtonWeapon");
     if(isCheckWeapon){
         Button.style.backgroundColor = "white";
@@ -24,7 +23,209 @@ function IAPbutton(){
         Button.style.backgroundColor = "lightgray";
     }
     isCheckWeapon = !isCheckWeapon;
-    /* Button.style.backgroundColor = (Button.style.backgroundColor == "lightgray") ? "white" : "lightgray"; */
+}
+/* Button.style.backgroundColor = (Button.style.backgroundColor == "lightgray") ? "white" : "lightgray"; */
+
+//アビリティ
+let isCheckAbility = true;
+function IAPbuttonAbility(){
+    var Button = document.getElementById("itemButtonAbility");
+    if(isCheckAbility){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckAbility = !isCheckAbility;
+}
+let isCheckSurvival = true;
+function IAPbuttonSurvival(){
+    var Button = document.getElementById("itemButtonSurvival");
+    if(isCheckSurvival){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckSurvival = !isCheckSurvival;
+}
+let isCheckCommon = true;
+function IAPbuttonCommon(){
+    var Button = document.getElementById("itemButtonCommon");
+    if(isCheckCommon){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckCommon = !isCheckCommon;
+}
+let isCheckRare = true;
+function IAPbuttonRare(){
+    var Button = document.getElementById("itemButtonRare");
+    if(isCheckRare){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckRare = !isCheckRare;
+}
+let isCheckEpic = true;
+function IAPbuttonEpic(){
+    var Button = document.getElementById("itemButtonEpic");
+    if(isCheckEpic){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckEpic = !isCheckEpic;
+}
+let isCheckLife = true;
+function IAPbuttonLife(){
+    var Button = document.getElementById("itemButtonLife");
+    if(isCheckLife){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckLife = !isCheckLife;
+}
+let isCheckArmor = true;
+function IAPbuttonArmor(){
+    var Button = document.getElementById("itemButtonArmor");
+    if(isCheckArmor){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckArmor = !isCheckArmor;
+}
+let isCheckShield = true;
+function IAPbuttonShield(){
+    var Button = document.getElementById("itemButtonShield");
+    if(isCheckShield){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckShield = !isCheckShield;
+}
+let isCheckWeaponPower = true;
+function IAPbuttonWeaponPower(){
+    var Button = document.getElementById("itemButtonWeaponPower");
+    if(isCheckWeaponPower){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckWeaponPower = !isCheckWeaponPower;
+}
+let isCheckAbilityPower = true;
+function IAPbuttonAbilityPower(){
+    var Button = document.getElementById("itemButtonAbilityPower");
+    if(isCheckAbilityPower){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckAbilityPower = !isCheckAbilityPower;
+}
+let isCheckAttackSpeed = true;
+function IAPbuttonAttackSpeed(){
+    var Button = document.getElementById("itemButtonAttackSpeed");
+    if(isCheckAttackSpeed){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckAttackSpeed = !isCheckAttackSpeed;
+}
+let isCheckCtReducation = true;
+function IAPbuttonCtReducation(){
+    var Button = document.getElementById("itemButtonCtReducation");
+    if(isCheckCtReducation){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckCtReducation = !isCheckCtReducation;
+}
+let isCheckAmmo = true;
+function IAPbuttonAmmo(){
+    var Button = document.getElementById("itemButtonAmmo");
+    if(isCheckAmmo){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckAmmo = !isCheckAmmo;
+}
+let isCheckWeapon_LifeSteal = true;
+function IAPbuttonWeapon_LifeSteal(){
+    var Button = document.getElementById("itemButtonWeapon_LifeSteal");
+    if(isCheckWeapon_LifeSteal){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckWeapon_LifeSteal = !isCheckWeapon_LifeSteal;
+}
+let isCheckAbility_LifeSteal = true;
+function IAPbuttonAbility_LifeSteal(){
+    var Button = document.getElementById("itemButtonAbility_LifeSteal");
+    if(isCheckAbility_LifeSteal){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckAbility_LifeSteal = !isCheckAbility_LifeSteal;
+}
+let isCheckSpeed = true;
+function IAPbuttonSpeed(){
+    var Button = document.getElementById("itemButtonSpeed");
+    if(isCheckSpeed){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckSpeed = !isCheckSpeed;
+}
+let isCheckReloadSpeed = true;
+function IAPbuttonReloadSpeed(){
+    var Button = document.getElementById("itemButtonReloadSpeed");
+    if(isCheckReloadSpeed){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckReloadSpeed = !isCheckReloadSpeed;
+}
+let isCheckMeleeDamage = true;
+function IAPbuttonMeleeDamage(){
+    var Button = document.getElementById("itemButtonMeleeDamage");
+    if(isCheckMeleeDamage){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckMeleeDamage = !isCheckMeleeDamage;
+}
+let isCheckCritical = true;
+function IAPbuttonCritical(){
+    var Button = document.getElementById("itemButtonCritical");
+    if(isCheckCritical){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckCritical = !isCheckCritical;
+}
+let isCheckOthers = true;
+function IAPbuttonOthers(){
+    var Button = document.getElementById("itemButtonOthers");
+    if(isCheckOthers){
+        Button.style.backgroundColor = "white";
+    }else{
+        Button.style.backgroundColor = "lightgray";
+    }
+    isCheckOthers = !isCheckOthers;
 }
 
 //タブ切り替え
