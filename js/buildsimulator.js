4b932695cae4e7ca857dee926abe8350dd2b2a33
RIN654xyz <rin654xyz@gmail.com>
2025-06-14 00:12:16 +0900

    ・パワーリスト追加 ・リストドロップダウン化 ・アイテムリストカテゴリー分け

--- js/buildsimulator.js
+++ js/buildsimulator.js
@@ -5,6 +5,9 @@
 // アイテムリストをテーブルに紐付け
 linkItemList(itemList);
 
+// パワーリストをテーブルに紐付け
+linkPowerList(powerList);
+
 // ------------------------------
 // 関数部
 // ------------------------------
@@ -24,9 +27,81 @@ function selectHero(){
     document.getElementById("herowindow").style.display = "none";
 }
 
+// アイテムリストを開く
+function openItemList(){
+    document.getElementById("itemlist-title-open").style.display = "block";
+    document.getElementById("itemlist-title-close").style.display = "none";
+    document.getElementById("itemlist").style.display = "block";
+}
+
+// アイテムリストを閉じる
+function closeItemList(){
+    document.getElementById("itemlist-title-open").style.display = "none";
+    document.getElementById("itemlist-title-close").style.display = "block";
+    document.getElementById("itemlist").style.display = "none";
+}
+
+// 武器リストを開く
+function openWeapon(){
+    document.getElementById("weapon-title-open").style.display = "block";
+    document.getElementById("weapon-title-close").style.display = "none";
+    document.getElementById("weapon").style.display = "block";
+}
+
+// 武器リストを閉じる
+function closeWeapon(){
+    document.getElementById("weapon-title-open").style.display = "none";
+    document.getElementById("weapon-title-close").style.display = "block";
+    document.getElementById("weapon").style.display = "none";
+}
+
+// アビリティリストを開く
+function openAbility(){
+    document.getElementById("ability-title-open").style.display = "block";
+    document.getElementById("ability-title-close").style.display = "none";
+    document.getElementById("ability").style.display = "block";
+}
+
+// アビリティリストを閉じる
+function closeAbility(){
+    document.getElementById("ability-title-open").style.display = "none";
+    document.getElementById("ability-title-close").style.display = "block";
+    document.getElementById("ability").style.display = "none";
+}
+
+// サバイバルリストを開く
+function openSurvival(){
+    document.getElementById("survival-title-open").style.display = "block";
+    document.getElementById("survival-title-close").style.display = "none";
+    document.getElementById("survival").style.display = "block";
+}
+
+// サバイバルリストを閉じる
+function closeSurvival(){
+    document.getElementById("survival-title-open").style.display = "none";
+    document.getElementById("survival-title-close").style.display = "block";
+    document.getElementById("survival").style.display = "none";
+}
+
+// パワーリストを開く
+function openPowerList(){
+    document.getElementById("powerlist-title-open").style.display = "block";
+    document.getElementById("powerlist-title-close").style.display = "none";
+    document.getElementById("powerlist").style.display = "block";
+}
+
+// パワーリストを閉じる
+function closePowerList(){
+    document.getElementById("powerlist-title-open").style.display = "none";
+    document.getElementById("powerlist-title-close").style.display = "block";
+    document.getElementById("powerlist").style.display = "none";
+}
+
 // アイテムリストをテーブルに紐づける関数
 function linkItemList(itemList) {
-    var tbody = document.getElementById("item-table").querySelector("tbody");
+    let tbody_weapon = document.getElementById("item-table-weapon").querySelector("tbody");
+    let tbody_ability = document.getElementById("item-table-ability").querySelector("tbody");
+    let tbody_survival = document.getElementById("item-table-survival").querySelector("tbody");
 
     // 各アイテムごとにループ
     for(let i=0; i<itemList.length; i++) {
@@ -38,6 +113,9 @@ function linkItemList(itemList) {
         let iconText = "-";  // アイコン列は現状アイテム情報にないため、とりあえずハイフンを入れる　TODO：RIN
         let statusText = "";
         let textText = "";
+
+        // カテゴリー判定用変数
+        let categoryCheck = "";
 
         // 各キーペアごとにループ
         Object.keys(itemList[i]).forEach(key => {
@@ -57,12 +135,12 @@ function linkItemList(itemList) {
 
                 // 値が0でない場合
                 if(itemList[i][key] != 0) {
-                    statusText = statusText + String(itemList[i][key]) + "\n";
+                    statusText = statusText + key + "+" + String(itemList[i][key]) + "\n";
                 }
             }
 
             // キー名がその他キーの場合
-            if(textKey == key) {
+            if(othersKey == key) {
 
                 // 値が"-"でない場合
                 if(itemList[i][key] != "-") {
@@ -78,40 +156,124 @@ function linkItemList(itemList) {
                 textText = itemList[i][key];
             }
 
+            // キー名がカテゴリーキーの場合
+            if(categoryKey == key) {
+
+                // カテゴリー判定用変数に値を代入
+                categoryCheck = itemList[i][key];
+            }
+
+        })
+
+        // 取得した各値をテーブルに紐付け
+        // カテゴリー別に紐付け先のテーブルを分ける
+        if(categoryCheck == "武器"){
+            tbody_weapon.appendChild(appendChildItemList(tr, isCheck, itemNameText, iconText, statusText, iconText));
+        }else if(categoryCheck == "アビリティ"){
+            tbody_ability.appendChild(appendChildItemList(tr, isCheck, itemNameText, iconText, statusText, iconText));
+        }else if(categoryCheck == "サバイバル"){
+            tbody_survival.appendChild(appendChildItemList(tr, isCheck, itemNameText, iconText, statusText, iconText));
+        }
+    }
+}
+
+// アイテムリスト用子要素作成関数（※カテゴリー別に分けているため関数化）
+function appendChildItemList(tr, isCheck, itemNameText, iconText, statusText, textText){
+    // 選択列
+    var td = document.createElement("td");
+    td.classList.add("item-td");
+    var input = document.createElement("input");
+    input.type = "checkbox";
+    input.checked = isCheck;
+    td.appendChild(input);
+    tr.appendChild(td);
+
+    // アイテム名列
+    var td = document.createElement("td");
+    td.textContent = itemNameText;
+    td.classList.add("item-td");
+    tr.appendChild(td);
+
+    // アイコン列
+    var td = document.createElement("td");
+    td.textContent = iconText;
+    td.classList.add("item-td");
+    tr.appendChild(td);
+
+    // ステータス列
+    var td = document.createElement("td");
+    td.innerHTML = statusText.replace(/\n/g, "<br>");
+    td.classList.add("item-td");
+    tr.appendChild(td);
+
+    // テキスト列
+    var td = document.createElement("td");
+    td.textContent = textText;
+    td.classList.add("item-td");
+    tr.appendChild(td);
+
+    return tr;
+}
+
+// パワーリストをテーブルに紐づける関数
+function linkPowerList(powerList) {
+    let tbody = document.getElementById("power-table").querySelector("tbody");
+
+    // 各パワーごとにループ
+    for(let i=0; i<powerList.length; i++) {
+        var tr = document.createElement("tr");
+
+        // 必要な列ごとの変数を初期化 
+        let isCheck = false;  // 選択列はパワー情報になく、初期時必ずfalseを入れる
+        let powerNameText = "";
+        let iconText = "-";  // アイコン列は現状パワー情報にないため、とりあえずハイフンを入れる　TODO：RIN
+        let textText = "";
+
+        // 各キーペアごとにループ
+        Object.keys(powerList[i]).forEach(key => {
+
+            // キー名がパワー名キーの場合
+            if(powerNameKey == key) {
+
+                // パワー名用変数に値を代入
+                powerNameText = powerList[i][key];
+            }
+
+            // キー名がテキストキーの場合
+            if(textKey == key) {
+
+                // テキスト用変数に値を代入
+                textText = powerList[i][key];
+            }
+
         })
 
         // 取得した各値をテーブルに紐付け
         // 選択列
         var td = document.createElement("td");
-        td.classList.add("item-td");
+        td.classList.add("power-td");
         var input = document.createElement("input");
         input.type = "checkbox";
         input.checked = isCheck;
         td.appendChild(input);
         tr.appendChild(td);
 
-        // アイテム名列
+        // パワー名列
         var td = document.createElement("td");
-        td.textContent = itemNameText;
-        td.classList.add("item-td");
+        td.textContent = powerNameText;
+        td.classList.add("power-td");
         tr.appendChild(td);
 
         // アイコン列
         var td = document.createElement("td");
         td.textContent = iconText;
-        td.classList.add("item-td");
-        tr.appendChild(td);
-
-        // ステータス列
-        var td = document.createElement("td");
-        td.innerHTML = statusText.replace(/\n/g, "<br>");
-        td.classList.add("item-td");
+        td.classList.add("power-td");
         tr.appendChild(td);
 
         // テキスト列
         var td = document.createElement("td");
         td.textContent = textText;
-        td.classList.add("item-td");
+        td.classList.add("power-td");
         tr.appendChild(td);
 
         tbody.appendChild(tr);
