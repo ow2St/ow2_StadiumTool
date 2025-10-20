892f412daf96a3c7076d815163ec51d1fcf36f78
aTsuji-vs <a.tsuji.ohf@gmail.com>
2025-10-21 00:22:26 +0900

    トップページに最新パッチ内容表示改修

--- js/toppage.js
+++ js/toppage.js
@@ -13,40 +13,15 @@ fetch("patchNoteData.json")
         return response.json();
     })
     .then(data => {
-        if (data.length > 0) {
-            const dates = [];
-
-            data.forEach(item => {
-                if (item.date) {
-                    // date型に変換して配列に格納
-                    dates.push(new Date(item.date));
-                }
-            });
-
-            // 最新日付を取得して文字列に変換
-            let latestDate = new Date(Math.max(...dates));
-            const year = latestDate.getFullYear();
-            const month = (latestDate.getMonth() + 1).toString().padStart(2, "0");
-            const day = latestDate.getDate().toString().padStart(2, "0");
-            const displayLatestDate = `${year}年${month}月${day}日`;
-            latestDate = `${year}/${month}/${day}`;
-
-            const link = document.createElement("a");
-            const latestDateSpan = document.createElement("span");
-
-            // URLエンコードして付与
-            link.href = `patchNote.html?date=${encodeURIComponent(latestDate)}&autoExpand=true`;
-            link.textContent = "最新パッチノート";
-            latestDateSpan.innerHTML = `（${displayLatestDate}）`;
-            linkContainer.appendChild(link);
-            linkContainer.appendChild(latestDateSpan);
-        } else {
-            linkContainer.textContent = "表示する最新パッチノートがありません。";
-        }
+        allData = data;
+        const result = organizeData(allData);
+        const latestData = result.latestData;
+        const latestDateString = result.latestDateString;
+        displayLatestData(latestData, latestDateString);
     })
     .catch(error => {
         console.error("データの読み込み中にエラーが発生しました:", error);
-        linkContainer.textContent = "リンクの生成に失敗しました。";
+        document.getElementById('latest-data-display').textContent = 'データの読み込みに失敗しました。';
     });
     
 // ------------------------------
